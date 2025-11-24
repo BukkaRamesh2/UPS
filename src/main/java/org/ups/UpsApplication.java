f401273584be5d9c1b3a402fce869aa3f3a7ef8e
BukkaRamesh <bukkaramesh.b@gmail.com>
2025-11-23 19:07:10 -0600

    Inital commit

---
+++ src/main/java/org/ups/UpsApplication.java
@@ -0,0 +1,74 @@
+package org.ups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * 
+ * 
+ * 
+ *    User managment  -- Ramesh
+ *    
+ *    
+ *    
+ *    
+ *    Shipping management --  Ahmed
+ *    
+ *    shipping Id 
+ *    senderAddress
+ *    reciverAddress
+ *    shipmentType
+ *    shipmentStatus
+ *    createdDate
+ *    estimatedDate
+ *    
+ *    
+ *    
+ *    Billing component   -- Venkat 
+ *    
+ *    
+ *    
+ *    
+ *    Package   -- shashank
+ *    
+ *    
+ *    
+ *    Inventory / Warehouse  -- Srujan 
+ *    
+ *    
+ *    
+ *    Notification --  vaishnavi
+ *    
+ *    
+ *    
+ *    
+ *    Customer support    --- veronica
+ *    
+ *    
+ *    
+ *    
+ *    
+ *    
+ *    Admin console
+ *    Fraud detection 
+ *    
+ *    
+ *    
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+@SpringBootApplication
+public class UpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpsApplication.class, args);
+	}
+
+}
