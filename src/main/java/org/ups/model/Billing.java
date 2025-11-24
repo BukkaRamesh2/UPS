36417782c2594a798016c4db1e6d5f3dec8bfe6e
svboy <svboy@Boyapati.home.local>
2025-11-24 06:45:22 -0600

    Billing class datatypes and variables work 21st nov work

---
+++ src/main/java/org/ups/model/Billing.java
@@ -0,0 +1,15 @@
+package org.ups.model;
+
+public class Billing {
+	int billingId;
+	int shippingId;
+	int userId;
+	String invoiceNumber;
+	double billingAmount;
+	double taxAmount;
+	double totalAmount;
+	String paymentMethod;   
+	String billingStatus;   
+	String createdDate;     
+	String dueDate;
+	}
