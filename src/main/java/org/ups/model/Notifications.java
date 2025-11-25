1413f718ac005c5058d34270667a0315bc90a17b
Vaishnavi Medishetty <vaishnavimedishetty@Mac.lan>
2025-11-24 21:21:45 -0600

    added variables to notifications class

--- src/main/java/org/ups/model/Notifications.java
+++ src/main/java/org/ups/model/Notifications.java
@@ -2,4 +2,13 @@ package org.ups.model;
 
 public class Notifications {
 
+	String userId;
+	String trackingNumber;
+	String message;
+	boolean isRead;
+	String driverId;
+	String estimatedDeliveryDate;
+	String currentStatus;
+	String packageLocation;
+	
 }
