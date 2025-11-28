27da1132afd7c740d3032832be4c2c2099724cca
Initial Commit <nsrujan.dev@gmail.com>
2025-11-28 00:01:13 -0600

    Implemented Inventory class with variables and constructors

--- src/main/java/org/ups/UpsApplication.java
+++ src/main/java/org/ups/UpsApplication.java
@@ -63,7 +63,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 
-
 @SpringBootApplication
 public class UpsApplication {
 
--- src/main/java/org/ups/model/Inventory.java
+++ src/main/java/org/ups/model/Inventory.java
@@ -6,15 +6,53 @@ import java.sql.Timestamp;
 
 public class Inventory {
 
-    long inventoryId;
-    String  supplierId;
-    String productName;
-    double Weight;
-    Enum weightUnit;
+    public long inventoryId;  // Public: Accessible anywhere
+    private String  supplierId;  // Private: Only accessible inside this class
+    protected String productName;  // Protected: Accessible in package + subclasses
+
+    // Default access (no keyword): Accessible only in package
+    double weight;
     boolean isFragile;
     int quantityOnHand;
     Timestamp createdDate;
     Date expirationDate;
 
+    enum WeightUnit{LBS, KG, OUNCES}
+    WeightUnit weightUnit;
 
+//Constructors
+
+//Constructor A : Default
+public Inventory() {
+    this.createdDate = new Timestamp(System.currentTimeMillis());
+}
+// Constructor B : Parameterized
+public Inventory(long inventoryId, String supplierId, String productName, double weight) {
+    this(); // Calls Constructor A
+    this.inventoryId = inventoryId;
+    this.supplierId = supplierId;
+    this.productName = productName;
+    this.weight = weight;
+}
+
+// Adding The Main Method:
+public static void main(String[] args) {
+    // Creating object using Parameterized Constructor
+    Inventory upsItem = new Inventory(101, "SUP-001", "Macbook Pro", 3.5);
+
+    //Setting Other Variables
+    upsItem.weightUnit = WeightUnit.LBS;
+    upsItem.isFragile = true;
+    upsItem.quantityOnHand = 10;
+
+    // Printing to console
+    System.out.println("--- UPS Inventory System ---");
+    System.out.println("ID: " + upsItem.inventoryId);
+    System.out.println("Product: " + upsItem.productName);
+
+    // We can access 'supplierId' here because main is INSIDE the Inventory class
+    System.out.println("Supplier: " + upsItem.supplierId);
+
+    System.out.println("Weight: " + upsItem.weight + " " + upsItem.weightUnit);
+    }
 }
