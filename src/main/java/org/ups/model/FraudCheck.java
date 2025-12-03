830f590cd4eeb58dc3901cfa775da36ce4d879d9
pulishashank8 <pulishashank8@gmail.com>
2025-12-02 23:30:07 -0500

    fraudOops

--- src/main/java/org/ups/model/FraudCheck.java
+++ src/main/java/org/ups/model/FraudCheck.java
@@ -1,7 +1,24 @@
 package org.ups.model;
 
 public class FraudCheck {
-    // Public variables (Parent Class)
-    public String ipAddress;
-    public String riskLevel;
+    private String ipAddress;     
+    protected String riskLevel;   
+
+    public FraudCheck() { }
+
+    public FraudCheck(String ipAddress, String riskLevel) {
+        this.ipAddress = ipAddress;
+        this.riskLevel = riskLevel;
+    }
+
+    public String getIpAddress() { return ipAddress; }
+    public void setIpAddress(String ipAddress) { this.ipAddress = ipAddress; }
+
+    public String getRiskLevel() { return riskLevel; }
+    public void setRiskLevel(String riskLevel) { this.riskLevel = riskLevel; }
+
+    @Override
+    public String toString() {
+        return "FraudCheck[ip=" + ipAddress + ", risk=" + riskLevel + "]";
+    }
 }
--- src/main/java/org/ups/model/FraudDetection.java
+++ src/main/java/org/ups/model/FraudDetection.java
@@ -1,27 +1,64 @@
 package org.ups.model;
 
-// 1. ADD "extends FraudCheck" HERE
 public class FraudDetection extends FraudCheck {
-
-    private int caseId;
+    private Integer caseId;       
     private String username;
     private double amount;
     private String location;
-    private boolean isBlocked;
+    public boolean isBlocked;     
+    
+    // Default constructor
+    public FraudDetection() {
+        super();
+        this.caseId = null;
+        this.username = "unknown";
+        this.amount = 0.0;
+        this.location = "none";
+        this.isBlocked = false;
+        this.count = 0;
+    }
 
-    public FraudDetection(int caseId, String username, double amount, String location, boolean isBlocked, String ipAddress) {
+    // Parameterized constructor
+    public FraudDetection(Integer caseId, String username, double amount, String location,
+                          boolean isBlocked, String ipAddress, String riskLevel) {
+        super(ipAddress, riskLevel); // initialize parent fields
         this.caseId = caseId;
         this.username = username;
         this.amount = amount;
         this.location = location;
         this.isBlocked = isBlocked;
-        
-        // 2. Initialize Parent Variable
-        this.ipAddress = ipAddress; 
+        this.count = 0;
     }
 
-    public void printFraudReport() {
-        System.out.println("User: " + username);
-        System.out.println("IP (From Parent): " + ipAddress);
+    // Overloaded constructor 
+    public FraudDetection(Integer caseId, String username) {
+        this(caseId, username, 0.0, "none", false, null, null);
+    }
+
+    // getters / setters 
+    public Integer getCaseId() { return caseId; }
+    public void setCaseId(Integer caseId) { this.caseId = caseId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    // Method overloading: two versions of check()
+    public boolean checkFraud() {
+        // simple rule
+        return this.amount > 5000.0;
+    }
+
+    public boolean checkFraud(double threshold) {
+        return this.amount > threshold;
+    }
+    @Override
+    public String toString() {
+        return "FraudDetection{caseId=" + caseId + ", username=" + username + ", amount=" + amount + ", location=" + location + ", blocked=" + isBlocked + "}";
     }
 }
