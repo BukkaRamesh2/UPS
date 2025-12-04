6d57812d3ff424837bc80cc95e59ac015c79fa12
shashankgoudm <shashank.male@lindsey.edu>
2025-12-04 02:13:57 -0600

    Updated UPS Package service and Implementation

--- src/main/java/org/ups/model/Package.java
+++ src/main/java/org/ups/model/Package.java
@@ -1,12 +1,69 @@
 package org.ups.model;
 
 public class Package {
-	
-	
-	public Long TrackingId;
-	public String orderId;
-	public String weight;
-	public String dimensions;
-	public String location;
-	
+
+    private long trackingId;
+    private String orderId;
+    private double weight;
+    private String dimensions;
+    private String location;
+
+    // Default constructor
+    public Package() {
+        this.trackingId = 0;
+        this.orderId = "Unknown";
+        this.weight = 0.0;
+        this.dimensions = "Unknown";
+        this.location = "Unknown";
+    }
+
+    // Parameter constructor
+    public Package(long trackingId, String orderId, double weight, String dimensions, String location) {
+        this.trackingId = trackingId;
+        this.orderId = orderId;
+        this.weight = weight;
+        this.dimensions = dimensions;
+        this.location = location;
+    }
+
+    // Getters & Setters
+    public long getTrackingId() {
+        return trackingId;
+    }
+
+    public void setTrackingId(long trackingId) {
+        this.trackingId = trackingId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
