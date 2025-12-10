b35768f86267051238ca41332c4ab0a72b09c667
shashankgoudm <shashank.male@lindsey.edu>
2025-12-10 02:08:57 -0600

    added custom exceptions and collections to package management

---
+++ src/main/java/org/ups/exception/PackageNotFoundException.java
@@ -0,0 +1,5 @@
+package org.ups.exception;
+
+public class PackageNotFoundException {
+
+}
--- src/main/java/org/ups/util/PackageTest.java
+++ src/main/java/org/ups/util/PackageTest.java
@@ -1,5 +1,46 @@
 package org.ups.util;
 
+import org.ups.model.Package;
+import org.ups.service.PackageServiceImpl;
+import org.ups.util.SenderComparator;
+
+import java.util.Collections;
+import java.util.List;
+
 public class PackageTest {
 
+    public static void main(String[] args) {
+
+        PackageServiceImpl service = new PackageServiceImpl();
+
+        Package p1 = new Package(1, "ORD001", 10.0, "10x10x10", "Dallas");
+        Package p2 = new Package(2, "ORD002", 5.0, "12x8x6", "Houston");
+        Package p3 = new Package(3, "ORD003", 8.0, "15x10x7", "Austin");
+
+
+        service.addPackage(p1);
+        service.addPackage(p2);
+        service.addPackage(p3);
+
+        // 1. LIST ALL
+        System.out.println("All Packages:");
+        service.getAllPackages().forEach(p -> System.out.println(p.getOrderId()));
+
+        // 2. SORT BY WEIGHT (Comparable)
+        System.out.println("\nSort by weight:");
+      
+
+        // 3. SORT BY SENDER NAME (Comparator)
+        System.out.println("\nSort by sender:");
+        List<Package> list = service.getAllPackages();
+        Collections.sort(list, new SenderComparator());
+        list.forEach(p -> System.out.println(p.getOrderId()));
+
+        // 4. Custom Exception Test
+        try {
+            service.deletePackage("1000"); // doesn't exist
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
 }
